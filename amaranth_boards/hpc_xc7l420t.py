1f32cf1caefe89e5e80515a8ba7639033a8e7355
Hans Baier <hansfbaier@gmail.com>
2022-05-11 04:34:56 +0700

    hpc-xc7k420t: add pcie, sfp and connectors, rename platform

--- amaranth_boards/hpc_xc7l420t.py
+++ amaranth_boards/hpc_xc7l420t.py
@@ -5,13 +5,13 @@ from amaranth.build import *
 from amaranth.vendor.xilinx import *
 from amaranth_boards.resources import *
 
-__all__ = ["HPCXC7K420TPlatform"]
+__all__ = ["HPCStoreXC7K420TPlatform"]
 
 """
 Board support for this chinese Kintex 420T board by "HPC FPGA Board Store"
 https://www.aliexpress.com/item/1005001631827738.html
 """
-class HPCXC7K420TPlatform(XilinxPlatform):
+class HPCStoreXC7K420TPlatform(XilinxPlatform):
     device      = "xc7k420t"
     package     = "ffg901"
     speed       = "2"
@@ -86,6 +86,29 @@ class HPCXC7K420TPlatform(XilinxPlatform):
                 Attrs(IOSTANDARD="SSTL15", SLEW="FAST"),
             ),
 
+            Resource("pcie", 0,
+                Subsignal("rst", PinsN("W21", dir="i"), Attrs(IOSTANDARD=self.get_lvcmos())),
+                Subsignal("clk", DiffPairs("T6", "T5", dir="i")),
+                Subsignal("tx", DiffPairs("N4 P2 T2 V2 Y2 AB2 AD2 AF2", "N3 P1 T1 V1 Y1 AB1 AD1 AF1", dir="o")),
+                Subsignal("rx", DiffPairs("P6 R4 U4 V6 W4  Y6 AA4 AB6", "P5 R3 U3 V5 W3  Y5 AA3 AB5",  dir="i")),
+            ),
+
+            Resource("sfp", 0,
+                Subsignal("tx", DiffPairs("A8",  "A7", dir="o")),
+                Subsignal("rx", DiffPairs("D10", "D9", dir="i")),
+                Subsignal("tx_disable", PinsN("A17", dir="o"), Attrs(IOSTANDARD=self.get_lvcmos())),
+                Subsignal("sda", Pins("C15", dir="io"),        Attrs(IOSTANDARD=self.get_lvcmos())),
+                Subsignal("scl", Pins("A15", dir="o"),         Attrs(IOSTANDARD=self.get_lvcmos())),
+            ),
+
+            Resource("sfp", 1,
+                Subsignal("tx", DiffPairs("C8",  "C7", dir="o")),
+                Subsignal("rx", DiffPairs("F10", "F9", dir="i")),
+                Subsignal("tx_disable", PinsN("D14", dir="o"), Attrs(IOSTANDARD=self.get_lvcmos())),
+                Subsignal("sda", Pins("C14", dir="io"),        Attrs(IOSTANDARD=self.get_lvcmos())),
+                Subsignal("scl", Pins("B14", dir="o"),         Attrs(IOSTANDARD=self.get_lvcmos())),
+            ),
+
             Resource("sata", 0,
                 Subsignal("tx", DiffPairs("A12", "A11", dir="o")),
                 Subsignal("rx", DiffPairs("C12", "C11", dir="i")),
@@ -101,12 +124,103 @@ class HPCXC7K420TPlatform(XilinxPlatform):
 
         super().__init__(toolchain=toolchain)
 
-
+    #
+    #         Connector layout on the board
+    #   ┌────────────────────────────────────────┐
+    #   │    2                            80     │
+    #   │    ┌──────────────────────────────┐    │
+    #   └──┐ └──────────────────────────────┘ ┌──┘
+    #      │ 1                            79  │
+    #      └──────────────────────────────────┘
+    #
     connectors  = [
-        #Connector("BTB", 0, {
-        #    # odd row     even row
-        #    # "7": "AD21",    "8": "AE21",
-        #})
+        # Connector on the SFP side
+        Connector("BTB", 0, {
+          # "1":  "GND",   "2": "GND",
+            "3":  "A16",   "4": "B24",
+            "5":  "B17",   "6": "D24",
+          # "7":  "GND",   "8": "GND",
+            "9":  "E16",  "10": "A14",
+            "11": "F16",  "12": "B15",
+            "13": "R25",  "14": "U30",
+            "15": "R24",  "16": "U29",
+          # "17": "GND",  "18": "GND",
+            "19": "R21",  "20": "T27",
+            "21": "R20",  "22": "R26",
+            "23": "T23",  "24": "U28",
+            "25": "R23",  "26": "U27",
+          # "27": "GND",  "28": "GND",
+            "29": "T18",  "30": "V25",
+            "31": "T17",  "32": "V24",
+            "33": "V20",  "34": "R19",
+            "35": "U20",  "36": "R18",
+          # "37": "GND",  "38": "GND",
+            "39": "W23",  "40": "T21",
+            "41": "W22",  "42": "T20",
+            "43": "U18",  "44": "V19",
+            "45": "U17",  "46": "U19",
+          # "47": "GND",  "48": "GND",
+            "49": "T26",  "50": "W17",
+            "51": "T25",  "52": "V17",
+            "53": "V22",  "54": "W19",
+            "55": "V21",  "56": "W18",
+          # "57": "GND",  "58": "GND",
+            "59": "C24",  "60": "T22",
+            "61": "D26",  "62": "V30",
+            "63": "C27",  "64": "U25",
+            "65": "B27",  "66": "AF25",
+          # "67": "GND",  "68": "GND"x
+            "69": "Y24",  "70": "AH26",
+            "71": "AE26", "72": "AG25",
+            "73": "AD26", "74": "AH25",
+          # "75": "GND",  "76": "GND"
+          # "77": "NC",   "78": "3V3"
+          # "79": "NC",   "80": "3V3"
+        }),
+
+        # Connector on the power side
+        Connector("BTB", 1, {
+          # "1":  "GND",    "2": "GND",
+            "3":  "AJ11",   "4": "AK9",
+            "5":  "AJ12",   "6": "AK10",
+          # "7":  "GND",    "8": "GND",
+            "9":  "AJ7",    "10": "AG11",
+            "11": "AJ8",    "12": "AG12",
+          # "13": "GND",    "14": "GND",
+            "15": "AF9",    "16": "AG7",
+            "17": "AF10",   "18": "AG8",
+          # "19": "GND",    "20": "GND",
+            "21": "AE11",   "22": "AH9",
+            "23": "AE12",   "24": "AH10",
+          # "25": "GND",    "26": "GND",
+            "27": "AE8",    "28": "AF6",
+            "29": "AE7",    "30": "AF5",
+          # "31": "GND",    "32": "GND",
+            "33": "AG3",    "34": "AK5",
+            "35": "AG4",    "36": "AK6",
+          # "37": "GND",    "38": "GND",
+            "39": "AE3",    "40": "AH5",
+            "41": "AE4",    "42": "AH6",
+          # "43": "GND",    "44": "GND",
+            "45": "AK1",    "46": "AJ3",
+            "47": "AK2",    "48": "AJ4",
+         #  "49": "GND",    "50": "GND",
+            "51": "AC3",    "52": "AH1",
+            "53": "AC4",    "54": "AH2",
+          # "55": "GND",    "56": "GND",
+            "57": "NC",     "58": "AC19",
+            "59": "L17",    "60": "AB19",
+          # "61": "GND",    "62": "GND",
+            "63": "AC20",   "64": "AB20",
+            "65": "AE20",   "66": "AA20",
+          # "67": "GND",    "68": "GND",
+            "69": "W24",    "70": "Y20",
+                            "72": "AA21",
+          # "73": "GND",    "74": "GND",
+          # "75": "NC",     "76": "GND",
+          # "77": "VCC12V", "78": "VCC3.3V",
+          # "79": "VCC12V", "80": "VCC3.3V",
+        }),
     ]
 
     def toolchain_prepare(self, fragment, name, **kwargs):
@@ -138,4 +252,4 @@ class HPCXC7K420TPlatform(XilinxPlatform):
 
 if __name__ == "__main__":
     from amaranth_boards.test.blinky import *
-    HPCXC7K420TPlatform().build(Blinky(), do_program=True)
+    HPCStoreXC7K420TPlatform().build(Blinky(), do_program=True)
